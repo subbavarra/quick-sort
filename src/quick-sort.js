82385c67b65d33a5a354fcdc7d7ba8357fea2ccc
subba_varra@epam.com <Bingo1234>
2017-07-12 15:14:26 +0530

    Quick Sort implementation By subba

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -2,5 +2,39 @@
 
 function quickSort(input) {
 
+ try { 
+    if(input.constructor === Array)
+    {
+
+      if (input.length > 1) {
+            var Pivot = input[0];
+
+              var lessThanPivot = [];
+              var greaterThanPivot = [];
+
+              for (var i = 1; i < input.length; i++) {
+                  if(input[i] < Pivot)
+                    lessThanPivot.push(input[i]);
+                  else
+                    greaterThanPivot.push(input[i]);
+              }
+
+           //var sortedArray=quickSort(lessThanPivot);
+            //sortedArray.concat(Pivot);
+           //sortedArray.concat(quickSort(greaterThanPivot));
+           //return sortedArray;
+          return quickSort(lessThanPivot).concat(Pivot).concat(quickSort(greaterThanPivot));
+        }
+        else
+        return input;
+    }
+       else
+       throw "Input is not an array"
+    }
+    catch(err) {
+       throw err;
+    }
+
+
 };
 module.exports = quickSort;
